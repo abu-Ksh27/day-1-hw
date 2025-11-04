8c41c62d6b47b5ff349ead0424af24ec2927546a
abu-Ksh27 <311823205002@msajce-edu.in>
2025-11-04 15:04:53 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,8 @@
+class Sum {
+    public static void main(String[] args) {
+        int num1 = 100;
+        int num2 = 200;
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
